7838f47a85f1ec1cd70fdb7d779f8033bc98c19c
nhim411 <lehoainam1998@gmail.com>
2022-05-09 14:33:43 +0700

    change api url

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -7,7 +7,7 @@ import queryString from 'query-string'
 // config` for the full list of configs
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://mock-api-2.herokuapp.com',
   timeout: 5000,
   paramsSerializer: (params) => queryString.stringify(params)
 })
